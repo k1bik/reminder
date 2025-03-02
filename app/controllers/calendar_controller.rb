a9fe3eb024732f4572ffb70937f2f4b2bf2e11fd
Kirill Biller <biller.k@yandex.ru>
2025-03-02 20:46:34 +0300

    Added reminders to calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,10 @@
 class CalendarController < ApplicationController
   def index
+    start_date = params.fetch(:start_date, Date.today).to_date
+    reminders = ReminderRecord.where(reminder_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+
+    respond_to do |f|
+      f.html { render :index, locals: { reminders: } }
+    end
   end
 end
--- app/models/reminder_record.rb
+++ app/models/reminder_record.rb
@@ -3,4 +3,12 @@ class ReminderRecord < ApplicationRecord
 
   validates :reminder_time, presence: true, comparison: { greater_than: -> { DateTime.current } }
   validates :text, presence: true
+
+  def start_time
+    reminder_time
+  end
+
+  def end_time
+    reminder_time + 300 # 5 мин
+  end
 end
